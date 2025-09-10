6cadf849058a9b7051c858b0b8e50854788c37c8
AlexanderOlegovichB <ip.boroday@gmail.com>
2025-09-10 19:00:43 +0300

    init

---
+++ src/test/java/PracticeFormPage.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.SelenideElement;
+
+
+public class PracticeFormPage {
+
+
+    private SelenideElement firstnameInput = $("#firstName");
+    private SelenideElement lastnameInput =
+    private SelenideElement emailInput =
+    private SelenideElement genderInput =
+    private SelenideElement mobileInput =
+    private SelenideElement birthdateInput =
+    private SelenideElement subjectsInput =
+    private SelenideElement hobbiesInput =
+    private SelenideElement currentadressInput =
+    private SelenideElement submitButton =
+
+
+}
